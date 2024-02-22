5c8df9661194fe1301f6deaf0216cce136b102c1
ShaneRRichards <your>
2024-02-21 23:35:46 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CS 230L</h1>
+      <h2>Section - 001</h2>
+      <p>WVU ID: 800375608</p>
+      <p>Hi I am Shane</p>
+      
+    </div>
+  );
+}
+
+export default App;
